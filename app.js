08c1a050260a12a20f3c3601eb420e9aae354d9c
= <=>
2018-12-17 16:17:53 -0300

    conexão com mongo db

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParse = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
+const Event = require('./models/event');
 const app = express();
-
-const events = [];
 
 app.use(bodyParse.json());
 
@@ -43,17 +43,33 @@ app.use(
         `),
         rootValue: {
             events: () => {
-                return events;
+                return Event.find()
+                    .then(events => {
+                        return events.map(event => {
+                            return { ...event._doc, _id: event._doc._id.toString() };
+                        });
+                    })
+                    .catch(err => {
+                        throw err;
+                    })
             },
             createEvent: args => {
-                const event = {
-                    _id: Math.random().toString(),
+                const event = new Event({   
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: args.eventInput.date
-                };
-                events.push(event);
+                    date: new Date(args.eventInput.date)
+                });
+                return event
+                    .save()
+                    .then(result => {
+                        console.log(result)
+                        return {...result._doc, _id: result._doc.id.toString() };
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        throw err;
+                    });
                 return event; 
             }
         },
@@ -61,4 +77,10 @@ app.use(
     })
 );
 
-app.listen(4200);
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-lvyha.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+    ).then(
+        () => { app.listen(4200); }
+    ).catch(
+        err => {console.log(err)}
+    )
---
+++ models/event.js
@@ -0,0 +1,24 @@
+const mongooose = require('mongoose');
+
+const Schema = mongooose.Schema;
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+});
+
+module.exports = mongooose.model('Event', eventSchema);
